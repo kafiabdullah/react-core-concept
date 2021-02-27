7d352aa3271c33f65f17dfa762c36e7b43086d46
kafi <abdullahkafi996@gmail.com>
2021-02-28 01:50:47 +0600

    create react component in App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  var person = {
-    name: "Md. Abdullah",
-    job: "Developer"
-  }
-  var style = {
-    color: "green"
-  }
-  return (
+// function App() {
+//   var person = {
+//     name: "Md. Abdullah",
+//     job: "Developer"
+//   }
+//   var style = {
+//     color: "green"
+//   }
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//         <h1 style={style}>Name: {person.name}</h1>
+//         <h2 style={{backgroundColor: "red"}}>Job: {person.job}</h2>
+//       </header>
+//     </div>
+//   );
+// }
+
+function App(){
+  return(
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1 style={style}>Name: {person.name}</h1>
-        <h2 style={{backgroundColor: "red"}}>Job: {person.job}</h2>
+        <h1>React Component Creating</h1>
+        <Person></Person>
       </header>
     </div>
-  );
+  )
 }
 
+
+function Person(){
+  let personStyle={
+    border: "2px solid green",
+    margin: "10px",
+    padding: "10px",
+    backgroundColor: "cyan",
+    color: "gray",
+    borderRadius: "10px"
+  }
+  return (
+  <div style={personStyle}> 
+    <h1>Name: Md.Abdullah</h1>
+    <h3>Web Developer</h3>
+  </div>
+  )
+}
 export default App;
